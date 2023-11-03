7ffdf1ebef9939d8f2f9666229c6c458dc58a5e2
Jarrod Rumsey <jrumsey@mun.ca>
2023-11-03 17:38:55 -0230

    Command line arguments added. -p: to change path of directory to sort. Defaults to user/downloads -nd: flag to disable newly added secondary confirmation dialog. Dialog allows user to verify sorted directory before sorting occurs.

--- DownloadOrganizerService.py
+++ DownloadOrganizerService.py
@@ -1,6 +1,10 @@
+import argparse
+from ast import parse
+from re import M
 import shutil
 
 from pathlib import Path
+from xmlrpc.client import Boolean
 
 def get_ext_name(ext):
     """
@@ -56,11 +60,45 @@ def sort_dir(dir_path):
     """
     for entry in dir_path.iterdir():
         sort_file(dir_path, entry) 
-           
+
+def dialog(folder_path): 
+    """Prompt the user for confirmation before starting the sort"""
+    choice = input(f"Are you sure you would like to sort the following directory?\nDirectory: {folder_path}\n(y/n): ").strip().lower()
+    return choice == 'y';
+   
 # Main Execution
+def main(folder_path: Path, no_dialog: bool):
+    
+    if no_dialog:
+        #sort_dir(folder_path)
+        print("SORT | ", folder_path)
+    else:
+        if dialog(folder_path):
+            #sort_dir(folder_path)
+            print("SORT CONFIRMED | ", folder_path)
+        else:
+            print("SORT DENIED")    
+            
+
+        
 if __name__ == "__main__":
-    DOWNLOADS_PATH = Path('C:/Users/Workplace/Downloads') 
-    sort_dir(DOWNLOADS_PATH)
+    DOWNLOADS_PATH = Path.home() / 'Downloads'
+    
+    parser = argparse.ArgumentParser(description="Sort all files in a given directory into subdirectories corresponding to file extension.")
+    
+    parser.add_argument('-p','--path', 
+                        type=Path, 
+                        help='The path to the directory with files to sort.',
+                        default=DOWNLOADS_PATH)
+    
+    parser.add_argument('-nd','--no_dialog', 
+                       action='store_true',
+                       help='Presence of flag disables secondary confirmation dialog',
+                       default=False)
+    
+    args = parser.parse_args()
+    
+    main(args.path, args.no_dialog)
 
 
 
